a921c1250a8aee8fcf31c5dff18a1fdea8a3fc4d
knuyg <uygunkn@gmail.com>
2023-11-19 16:59:17 +0100

    added option to show/hide raw data

--- StreamlitChurn/pages/1_Explore_Data.py
+++ StreamlitChurn/pages/1_Explore_Data.py
@@ -14,17 +14,20 @@ st.sidebar.header("Explore Dataset")
 # Load the data
 data = load_data()
 
-# User Control for number of rows to display
-rows_option = st.slider('Select number of rows to display', min_value=5, max_value=100, value=25, step=5)
-st.write(f"Displaying {rows_option} rows of data")
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
 
-# Pagination
-page_size = rows_option
-page_number = st.number_input('Page Number', min_value=1, max_value=(len(data) // page_size) + 1, step=1, value=1)
+    # User Control for number of rows to display
+    rows_option = st.slider('Select number of rows to display', min_value=5, max_value=100, value=25, step=5)
+    st.write(f"Displaying {rows_option} rows of data")
 
-# Calculate the start and end indices of the current page
-start_idx = (page_number - 1) * page_size
-end_idx = start_idx + page_size
+    # Pagination
+    page_size = rows_option
+    page_number = st.number_input('Page Number', min_value=1, max_value=(len(data) // page_size) + 1, step=1, value=1)
 
-# Display the data for the current page
-st.table(data.iloc[start_idx:end_idx])
+    # Calculate the start and end indices of the current page
+    start_idx = (page_number - 1) * page_size
+    end_idx = start_idx + page_size
+
+    # Display the data for the current page
+    st.table(data.iloc[start_idx:end_idx])
