b4622967663beb6ff397789ec318e06d2cf6a7d9
knuyg <uygunkn@gmail.com>
2023-11-19 17:03:29 +0100

    renamed to Homepage

---
+++ StreamlitChurn/Homepage.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Customer Churn Data Project")
+st.markdown("# Home")
+st.sidebar.header("Home")
+st.sidebar.success("Select a page above.")
--- StreamlitChurn/app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Home",
-)
-
-st.title("Streamlit Example")
