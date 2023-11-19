2335d6894608689257ec477c97f004cf53a2d9d2
knuyg <uygunkn@gmail.com>
2023-11-19 23:14:35 +0100

    added ML model selection in the sidebar

--- StreamlitChurn/pages/2_ML_Model.py
+++ StreamlitChurn/pages/2_ML_Model.py
@@ -1,4 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-st.table(data)
+st.set_page_config(page_title="ML Model", page_icon="📈")
+
+st.markdown("# Machine Learning Model Selection")
+st.sidebar.header("Select Model")
+
+# Fetch data from session state
+data = st.session_state.data
+
+classifier_name = st.sidebar.selectbox("Select Classifier", ("KNN", "SVM", "Random Forest"))
