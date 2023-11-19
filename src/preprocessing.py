5365a955aa5ef985ef5fb0fcbf2cc75a9efe780f
knuyg <uygunkn@gmail.com>
2023-11-20 00:26:56 +0100

    added ML steps files in the src folder

---
+++ src/preprocessing.py
@@ -0,0 +1,31 @@
+## Function to rename properly the columns
+def rename_features(feature_name: str) -> str:
+    """Properly renames the features of a given dataset
+
+    Parameters
+    ----------
+    file_loc : str
+        The file location of the spreadsheet
+    print_cols : bool, optional
+        A flag used to print the columns to the console (default is False)
+
+    Returns
+    -------
+    str
+        a list of strings representing the header columns
+    """
+
+
+    # Get rid of the leading and trailing whitespaces
+    new_name = feature_name.strip()
+
+    # Lower case only
+    new_name = new_name.lower()
+
+    # Replace the double spaces with only one
+    new_name = new_name.replace('  ', ' ')
+
+    # Finally replace the spaces with underscores
+    new_name = new_name.replace(' ', '_')
+
+    return new_name
