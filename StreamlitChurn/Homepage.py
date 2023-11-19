dc5a67b126607d0e0970708c6ff63635b2c900f1
knuyg <uygunkn@gmail.com>
2023-11-19 22:55:36 +0100

    default layout to wide

--- StreamlitChurn/Homepage.py
+++ StreamlitChurn/Homepage.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Customer Churn Data Project")
-st.markdown("# Home")
-st.sidebar.header("Home")
-st.sidebar.success("Select a page above.")
+st.set_page_config(page_title='Customer Churn Data Project', layout='wide')
+st.markdown('# Home')
+st.sidebar.header('Home')
+st.sidebar.success('Select a page above.')
